f7698317afe12fd6e79e3695607cf8de219bb11e
deep-git <deepakthangella5@gmail.com>
2024-02-25 22:59:58 -0500

    Fix image path

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ function App() {
   return (
     <div className="relative bg-primary_paleblue w-full min-h-screen font-redhat flex justify-center items-center">
       <picture>
-        <source media="(min-width: 1024px)" srcSet="/pattern-background-desktop.svg"/>
-        <img src="/pattern-background-mobile.svg" alt="Curved wave background" className="absolute z-10 top-0 left-0 right-0 w-full -my-[20px] h-1/3 md:h-1/2"/>
+        <source media="(min-width: 1024px)" srcSet="/order-summary/pattern-background-desktop.svg"/>
+        <img src="/order-summary/pattern-background-mobile.svg" alt="Curved wave background" className="absolute z-10 top-0 left-0 right-0 w-full -my-[20px] h-1/3 md:h-1/2"/>
       </picture>
 
       <main className="z-20 w-[325px] rounded-2xl bg-white overflow-hidden text-center shadow-[0px_10px_20px_rgba(56,41,224,0.2)] md:w-[400px]">
-        <img src="/illustration-hero.svg" alt="Illustration hero image on top of card"/>
-        
+        <img src="/order-summary/illustration-hero.svg" alt="Illustration hero image on top of card"/>
+
         <section className="mx-6 my-8 md:m-10">
           <h1 className="text-2xl text-darkblue font-[900]">Order Summary</h1>
           <p className="text-md mt-4 text-desaturated_blue font-[500] md:text-sm">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
 
           <div className="flex mt-5 p-4 bg-neutral_paleblue rounded-lg justify-between items-center md:p-6">
             <div className="flex">
-              <img src="/icon-music.svg" alt="Music icon"/>
+              <img src="/order-summary/icon-music.svg" alt="Music icon"/>
 
               <div className="ml-4 text-center flex flex-col justify-center">
                 <p className="text-darkblue font-[900] text-sm md:text-md">Annual Plan</p>
